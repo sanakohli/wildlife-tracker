00f9ea7c30dee9d09031ba069aff0466e9227775
Raina Parikh <rainaparikh@utexas.edu>
2025-02-08 13:25:43 -0600

    map changes

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./Map.jsx";
+
+function App() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Map.jsx
@@ -0,0 +1,23 @@
+// src/Map.js
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function Map() {
+  const position = [30.2672, -97.7431];
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
+    </div>
+  );
+}
