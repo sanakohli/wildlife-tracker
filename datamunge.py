df14777fcefbd940a86735ca8b0476f6f6af0050
Michelle Nguyen <michellehn@utexas.edu>
2025-02-09 06:25:52 -0600

    update model

--- datamunge.py
+++ datamunge.py
@@ -1,5 +1,4 @@
 # %%
-from itertools import count
 import geopandas as gpd # this library allows us to load .shp file into pandas df
 import matplotlib.pyplot as plt
 
@@ -20,10 +19,11 @@ for col in county_pop.columns:     # drop columns that arent for this ssp
             county_pop = county_pop.drop(columns=col)
 print(county_pop.head().to_string())  # View the first few rows of the dataset
 print(county_pop.crs)     # Check the coordinate reference system (CRS)
+county_pop = county_pop[['STATEFP10','NAME10','NAMELSAD10','ssp225']]
 # Plot the shapefile
-county_pop.plot()  # Basic plot
-plt.title("County Population Data")
-plt.show()
+# county_pop.plot()  # Basic plot
+# plt.title("County Population Data")
+# plt.show()
 
 # %%
 import geopandas as gpd # this library allows us to load .shp file into pandas df
@@ -34,6 +34,8 @@ from tqdm.auto import tqdm
 import plotly.express as px
 tqdm.pandas()
 
+# LOAD IN COUNTY GEOMETRY AND SPECIES DATA, MERGE ON LONG LAT IN COUNTY GEOM
+
 shapefile_path = "popdynamics-us-county-level-pop-projections-sex-race-age-ssp-2020-2100-pop-total-sex-race-shp/county_projectios_Total_bySex_byRace/total_pop_proj/hauer_county_totpop_SSPs.shp"
 counties = gpd.read_file(shapefile_path)
 counties = counties[['NAME10','NAMELSAD10','STATEFP10','geometry']]
@@ -41,96 +43,105 @@ state_fips = pd.read_csv('data/state_fips.csv')
 # print('counties df\n',counties.head().to_string())
 
 species_data = pd.read_csv('data/species_data.csv', sep='\t')
+species_data = species_data[['gbifID','species','countryCode','stateProvince','individualCount','decimalLatitude','decimalLongitude','year']]
+print(species_data.head().to_string())
 print('rows species data',len(species_data))    # Number of rows
 species_data = gpd.GeoDataFrame(species_data, geometry=gpd.points_from_xy(species_data.decimalLongitude, species_data.decimalLatitude), crs=counties.crs)
-# print('species data \n',species_data.head().to_string())
 species_county = gpd.sjoin(species_data, counties, how="left")
+print(species_county.head().to_string())
 
-species_county['STATEFP10'] = species_county['STATEFP10'].fillna(0).astype(int)
-species_county = species_county.merge(state_fips, left_on='STATEFP10', right_on='st')
-non_match = species_county[species_county['stname'] != species_county['stateProvince']]
-# print('merge \n',species_county[['stname','stateProvince']].head().to_string())
+# %%
+species_county_merge = species_county.copy()
+species_county_merge['STATEFP10'] = species_county_merge['STATEFP10'].fillna(0).astype(int)
+species_county_merge = species_county_merge.merge(state_fips, left_on='STATEFP10', right_on='st')
+non_match = species_county_merge[species_county_merge['stname'] != species_county_merge['stateProvince']]
+# print('merge \n',species_county_merge[['stname','stateProvince']].head().to_string())
 print('non matching states\n',non_match.head().to_string())
-species_county.to_csv('data/species_county_dirty.csv') # merged but lots of non important columns
+species_county_merge.to_csv('data/species_county_dirty.csv') # merged but lots of non important columns
+# %%
+# stname is based on lat and l=ong so more accurate
+# stateProvince is direct from gbif data
+species_county_merge.rename(columns={'stname':'state_geo','stateProvince':'state_obs'}, inplace=True)
+print('state_geo null', species_county_merge['state_geo'].isnull().sum())
+print('STATEFP10 null', species_county_merge['STATEFP10'].isnull().sum())
+print('state_obs null', species_county_merge['state_obs'].isnull().sum())
 # %%
 
-county_species = species_county.groupby(['NAME10','year','stateProvince']).size().reset_index(name='species')
-county_species = county_species[county_species['stateProvince'].str.len() > 2]
-
-county_species = county_species[~county_species['stateProvince'].str.contains('\(')]
-county_species = county_species[~county_species['stateProvince'].str.contains('\/')]
-county_species['stateProvince'] = county_species['stateProvince'].str.lower()
-county_species['stateProvince'] = county_species['stateProvince'].str.replace('main hawaiian islands','hawaii')
-county_species['stateProvince'] = county_species['stateProvince'].str.replace('oahu island','hawaii')
-county_species['stateProvince'] = county_species['stateProvince'].str.replace('hawaii state','hawaii')
-county_species['stateProvince'] = county_species['stateProvince'].str.replace('hawai`i','hawaii')
-
-
-
-print(county_species['stateProvince'].unique())
+county_species = species_county_merge.groupby(['NAME10','year','STATEFP10']).size().reset_index(name='species')
+print('county_species df \n',county_species.head().to_string())
+# print('unique state fips',county_species['STATEFP10'].unique())
+# print(f'len unique fips {len(county_species["STATEFP10"].unique())}')
 county_species.to_csv('data/county_species_annual.csv')
-county_species['ct_state'] = county_species['NAME10'] +'--' + county_species['stateProvince']
+county_species['ct_state'] = county_species.apply(lambda x: f"{x['NAME10']}--{x['STATEFP10']}", axis=1)
 yearly_change = []
 # print(county_species.to_string())
 for county_state in county_species['ct_state'].unique():
     county = county_state.split('--')[0]
-    state = county_state.split('--')[1]
+    state_fips = county_state.split('--')[1]
     county_data = county_species[county_species['ct_state'] == county_state]
     county_data = county_data.sort_values('year')
     for year in range(2015,2025):
         if year in county_data['year'].values: # TODO: fill in gaps of each year by doing mean of previous and next year
-            county_row = {'county':county, 'state':county_data['stateProvince'].values[0]}
+            county_row = {'county':county, 'state_fips':county_data['STATEFP10'].values[0], 'num_obs_endangered':county_data['species'].values[0]}
             county_row['base_year'] = year
             species_yr = county_data[county_data['year'] == year]['species'].values[0]
             year_minus5 = year - 5
+            year_minus3 = year - 3
             year_minus1 = year - 1
             year_plus5 = year + 5
-            if year_minus5 in county_data['year'].values:
-                county_row['5_yr_change_species'] = (species_yr - county_data[county_data['year'] == year_minus5]['species'].values[0]) / county_data[county_data['year'] == year_minus5]['species'].values[0]
-            else:
-                county_row['5_yr_change_species'] = None
-            if year_minus1 in county_data['year'].values:
-                county_row['1_yr_change_species'] = (species_yr - county_data[county_data['year'] == year_minus1]['species'].values[0]) / county_data[county_data['year'] == year_minus1]['species'].values[0]
-            else:
-                county_row['1_yr_change_species'] = None
-            if year_plus5 in county_data['year'].values:
-                species_2021 = county_data[county_data['year'] == year_plus5]['species'].values[0]
-                county_row['future_5_yr_change_species'] = (species_2021 - species_yr) / species_yr
-            else:
-                county_row['future_5_yr_change_species'] = None
-            yearly_change.append(county_row)
+            year_plus3 = year + 3
+            for year_diff in [year_minus5, year_minus3, year_minus1]:
+                diff = year - year_diff
+                if year_diff in county_data['year'].values:
+                    diff_species = county_data[county_data['year'] == year_diff]['species'].values[0]
+                    county_row[f'{diff}_yr_change_species'] = (species_yr - diff_species) / diff_species
+                else:
+                    county_row[f'{diff}_yr_change_species'] = None
+            for year_diff in [year_plus5, year_plus3]:
+                diff = year_diff - year
+                if year_diff in county_data['year'].values:
+                    diff_species = county_data[county_data['year'] == year_diff]['species'].values[0]
+                    county_row[f'future_{diff}_yr_change_species'] = (diff_species - species_yr) / species_yr
+                else:
+                    county_row[f'future_{diff}_yr_change_species'] = None
+            if len(county_row.keys()) > 3:
+                yearly_change.append(county_row)
 yearly_change = pd.DataFrame(yearly_change)
 print(f'len {len(yearly_change)}')
 print(f'num unique counties {len(yearly_change["county"].unique())}')
 print(yearly_change.head().to_string())
-print(yearly_change.groupby(['county', 'state']).ngroups)
+print('unique county state combos',yearly_change.groupby(['county', 'state_fips']).ngroups)
 # %%
 county_pop = pd.read_csv('data/county_populations.csv')
-county_pop = county_pop[['STNAME_2019', 'CTYNAME','CENSUS2010POP','POPESTIMATE2011','POPESTIMATE2012','POPESTIMATE2013','POPESTIMATE2014','POPESTIMATE2015','POPESTIMATE2016','POPESTIMATE2017','POPESTIMATE2018','POPESTIMATE2019','ESTIMATESBASE2020','POPESTIMATE2021','POPESTIMATE2022','POPESTIMATE2023']]
+county_pop = county_pop[['STNAME_2019','STATE_2019', 'CTYNAME','CENSUS2010POP','POPESTIMATE2011','POPESTIMATE2012','POPESTIMATE2013','POPESTIMATE2014','POPESTIMATE2015','POPESTIMATE2016','POPESTIMATE2017','POPESTIMATE2018','POPESTIMATE2019','ESTIMATESBASE2020','POPESTIMATE2021','POPESTIMATE2022','POPESTIMATE2023']]
 county_pop.rename(columns={'STNAME_2019':'STNAME','CENSUS2010POP':'2010POP','ESTIMATESBASE2020':'2020POP'}, inplace=True)
+
 county_pop['CTYNAME'] = county_pop['CTYNAME'].str.replace(' County','')
 for col in county_pop.columns:
     if 'POPESTIMATE' in col:
         county_pop = county_pop.rename(columns={col:f'{col[11:]}POP'})
-# print(county_pop.head().to_string())
+
 yearly_change_pop = []
 for county in county_pop['CTYNAME'].unique():
     county_data = county_pop[county_pop['CTYNAME'] == county]
     for year in range(2015,2024):
-        county_row = {'county':county, 'base_year':year,'state': county_data['STNAME'].values[0].lower()}
-        year_minus5 = year - 5
-        year_minus1 = year - 1
-        pop_yr = county_data[f'{year}POP'].values[0]
-        county_row['5_yr_change_pop'] = (pop_yr - county_data[f'{year_minus5}POP'].values[0]) / county_data[f'{year_minus5}POP'].values[0]
-        county_row['1_yr_change_pop'] = (pop_yr - county_data[f'{year_minus1}POP'].values[0]) / county_data[f'{year_minus1}POP'].values[0]
+        county_row = {'county':county, 'base_year':year,'state_fips': county_data['STATE_2019'].values[0]}
+        # population of county in year
+        county_yr_pop = county_data[f'{year}POP'].values[0]
+        for diff in [1, 3 ,5]:
+            year_minus_diff = year - diff
+            pop_diff_yr = county_data[f'{year_minus_diff}POP'].values[0] # population of county in year minus diff
+            county_row[f'{diff}_yr_change_pop'] = (county_yr_pop - pop_diff_yr) / pop_diff_yr
+            
         yearly_change_pop.append(county_row)
 
 yearly_change_pop = pd.DataFrame(yearly_change_pop)
 print(yearly_change_pop.head().to_string())
 # %%
-input_df = yearly_change.merge(yearly_change_pop, on=['county','state','base_year'])
+input_df = yearly_change.merge(yearly_change_pop, on=['county','state_fips','base_year'])
 print(f'len {len(input_df)}')
 print(f'num unique counties {len(input_df["county"].unique())}')
+print(f'num unique county state combos {input_df.groupby(["county", "state_fips"]).ngroups}')
 print(input_df.head(100).to_string())
 input_df.to_csv('data/input_df.csv', index=False)
 # %%
--- model.py
+++ model.py
@@ -1,63 +1,146 @@
 # %%
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import json
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
 input_df = pd.read_csv('data/input_df.csv')
+input_df['num_obs_endangered_pctl'] = input_df['num_obs_endangered'].rank(
+    pct=True)
 
-print('len',len(input_df))
-input_df.dropna(inplace=True)
-input_df['future_5_yr_change_species'] = input_df['future_5_yr_change_species'] * 100
-input_df['5_yr_change_species'] = input_df['5_yr_change_species'] * 100
-input_df['1_yr_change_species'] = input_df['1_yr_change_species'] * 100
-input_df['5_yr_change_pop'] = input_df['5_yr_change_pop'] * 100
-input_df['1_yr_change_pop'] = input_df['1_yr_change_pop'] * 100
+print('len', len(input_df))
+for col in input_df.columns:
+    if 'yr_change' in col:
+        input_df[col] = input_df[col] * 100
 print(input_df.head().to_string())
-print('len',len(input_df))
+print('len input df', len(input_df))
 
 
+X = input_df[['5_yr_change_species', '1_yr_change_species',
+              '5_yr_change_pop', '1_yr_change_pop', 'future_5_yr_change_species']]
+X.dropna(inplace=True)
+y = X['future_5_yr_change_species']
+X.drop(columns=['future_5_yr_change_species'], inplace=True)
 
-X = input_df[['5_yr_change_species','1_yr_change_species','5_yr_change_pop','1_yr_change_pop']]
-y = input_df['future_5_yr_change_species']
+print(f'std dev of future 5 yr change species {y.std()}')
 reg = LinearRegression().fit(X, y)
-coef = pd.DataFrame({'feature':X.columns,'coef':reg.coef_})
+coef = pd.DataFrame({'feature': X.columns, 'coef': reg.coef_})
 print(coef.head().to_string())
 rmse = ((reg.predict(X) - y) ** 2).mean() ** .5
-print(f'rmse {rmse}')
+print(f'rmse using 5 and 1 yr changes, pred 5 yr change{rmse}')
 
-X = input_df[['1_yr_change_species','1_yr_change_pop']]
-y = input_df['future_5_yr_change_species']
+X = input_df[['5_yr_change_species', '1_yr_change_species', '5_yr_change_pop',
+              '3_yr_change_pop', '1_yr_change_pop', 'future_3_yr_change_species']]
+X.dropna(inplace=True)
+y = X['future_3_yr_change_species']
+X.drop(columns=['future_3_yr_change_species'], inplace=True)
+
+print(f'std dev of future 3 yr change species {y.std()}')
+reg = LinearRegression().fit(X, y)
+coef = pd.DataFrame({'feature': X.columns, 'coef': reg.coef_})
+print(coef.head().to_string())
+rmse = ((reg.predict(X) - y) ** 2).mean() ** .5
+print(f'rmse using 5 ,3 and 1 yr changes, pred 3 yr change {rmse}')
+
+
+X = input_df[['1_yr_change_species',
+              '1_yr_change_pop', 'future_5_yr_change_species']]
+X.dropna(inplace=True)
+y = X['future_5_yr_change_species']
+X = X[['1_yr_change_species', '1_yr_change_pop']]
 reg2 = LinearRegression().fit(X, y)
-coef = pd.DataFrame({'feature':X.columns,'coef':reg2.coef_})
+coef = pd.DataFrame({'feature': X.columns, 'coef': reg2.coef_})
+rmse = ((reg2.predict(X) - y) ** 2).mean() ** .5
+print(f'rmse using 1 yr changes {rmse}')
 # print(coef.head().to_string())
 
 # %%
-input_df = pd.read_csv('data/input_df.csv')
-input_df['5_yr_change_species'] = input_df['5_yr_change_species'] * 100
-input_df['1_yr_change_species'] = input_df['1_yr_change_species'] * 100
-input_df['5_yr_change_pop'] = input_df['5_yr_change_pop'] * 100
-input_df['1_yr_change_pop'] = input_df['1_yr_change_pop'] * 100
-pred_df = input_df[input_df['base_year'] == 2023]
-print(f'len pred df {len(pred_df)}')
-pred_df.dropna(subset=['5_yr_change_species','1_yr_change_species','5_yr_change_pop','1_yr_change_pop'], inplace=True)
-X_pred = pred_df[['5_yr_change_species','1_yr_change_species','5_yr_change_pop','1_yr_change_pop']]
-print(f'len pred df {len(X_pred)}')
 
-y_pred = reg.predict(X_pred)
-pred_df['predicted_5_yr_change_species'] = y_pred
-pred_df['danger_score'] = pred_df['predicted_5_yr_change_species'].rank(pct=True, ascending=False)
-print(pred_df.to_string())
+# random_grid = {'bootstrap': [True, False],
+#                'max_depth': [2,3,4],
+#                'max_features': ['auto', 'sqrt'],
+#                'min_samples_leaf': [1, 2, 4],
+#                'min_samples_split': [2, 5, 10],
+#                'n_estimators': [100, 130, 150]}
+
+X_rf = input_df[['5_yr_change_species', '1_yr_change_species', '5_yr_change_pop',
+                 '1_yr_change_pop', '3_yr_change_species', '3_yr_change_pop', 'future_5_yr_change_species']]
+X_rf.dropna(inplace=True)
+y_rf = X_rf['future_5_yr_change_species']
+X_rf.drop(columns=['future_5_yr_change_species'], inplace=True)
+print(f'len X_rf {len(X_rf)}')
+X_train, X_test, y_train, y_test = train_test_split(
+    X_rf, y_rf, test_size=0.3, random_state=42)
+rf = RandomForestRegressor(
+    n_estimators=70, random_state=42, max_depth=3, min_samples_split=2)
+# rf = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+rmse = root_mean_squared_error(y_test, y_pred)
+print(f'std dev of future 5 yr change species {y_rf.std()}')
+print(f'rmse random forest {rmse}')
+
+gb = GradientBoostingRegressor(
+    n_estimators=70, random_state=42, learning_rate=0.01, max_depth=3)
+gb.fit(X_train, y_train)
+y_pred = gb.predict(X_test)
+rmse = root_mean_squared_error(y_test, y_pred)
+print('rmse gradient boosting', rmse)
+
+# %%
+
+
+def gen_out_json(model, features, y_name, obs_weight=.3, growth_weight=.7):
+    input_df = pd.read_csv('data/input_df.csv')
+    # print(input_df.isnull().sum())
+    for col in input_df.columns:
+        if 'yr_change' in col:
+            input_df[col] = input_df[col] * 100
+    pred_df = input_df[input_df['base_year'] == 2023]
+    print(f'len pred df {len(pred_df)}')
+    pred_df.dropna(subset=features, inplace=True)
+    X_pred = pred_df[features]
+    print(f'len pred df {len(X_pred)}')
+
+    y_pred = model.predict(X_pred)
+    pred_df[y_name] = y_pred
+    pred_df['danger_score'] = (pred_df[y_name] - pred_df[y_name].mean()) / \
+        pred_df[y_name].std(ddof=0)
+    pred_df['num_obs_z'] = (pred_df['num_obs_endangered'] -
+                            pred_df['num_obs_endangered'].mean()) / pred_df['num_obs_endangered'].std(ddof=0)
+    pred_df['danger_score'] = obs_weight * pred_df['num_obs_z'] + pred_df['danger_score'] * growth_weight
+    pred_df['danger_score'] = pred_df['danger_score'].rank(pct=True, ascending=False)
+    pred_df['county_state'] = pred_df.apply(
+        lambda x: f"{x['county']}_{x['state_fips']}", axis=1)
+    print(pred_df.head().to_string())
+    out = {}
+    for row in pred_df.itertuples():
+        out[row.county_state] = row.danger_score
+    with open('data/pred.json', 'w') as json_file:
+        json.dump(out, json_file, indent=4)
+    with open('frontend/src/data/pred.json', 'w') as json_file:
+        json.dump(out, json_file, indent=4)
+
+
+gen_out_json(gb, ['5_yr_change_species', '1_yr_change_species', '5_yr_change_pop',
+             '1_yr_change_pop', '3_yr_change_species', '3_yr_change_pop'], 'future_3_yr_change_species')
 # %%
 
 state_fips = pd.read_json('data/state_mapping_fips.json', orient='index')
 state_fips.reset_index(inplace=True)
-state_fips.columns = ['fips','state']
+state_fips.columns = ['fips', 'state']
 state_fips['state'] = state_fips['state'].str.lower()
 pred_df2 = pred_df.merge(state_fips, on='state')
-pred_df2['county_state'] = pred_df2.apply(lambda x: f"{x['county']}_{x['fips']}", axis=1)
+pred_df2['county_state'] = pred_df2.apply(
+    lambda x: f"{x['county']}_{x['fips']}", axis=1)
 print(pred_df2.to_string())
 # print(pred_df2[pred_df2['fips'].isnull()])
-print('num null fips ',len(pred_df2[pred_df2['fips'].isnull()]))
-pred_df2 = pred_df2[['county_state','danger_score']]
+print('num null fips ', len(pred_df2[pred_df2['fips'].isnull()]))
+pred_df2 = pred_df2[['county_state', 'danger_score']]
 out = {}
 for row in pred_df2.itertuples():
     out[row.county_state] = row.danger_score
